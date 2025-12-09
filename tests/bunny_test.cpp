18d4320e0030d308a0e0235560df7a6a6ff0addd
torhankarina <torhankarina@gmail.com>
2025-12-08 23:01:45 -0600

    added bunny to cmakelist

---
+++ tests/bunny_test.cpp
@@ -0,0 +1,6 @@
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/benchmark/catch_benchmark.hpp>
+#include <catch2/benchmark/catch_constructor.hpp>
+#include <catch2/generators/catch_generators_range.hpp>
+
+#include "../src/bunny.hpp"
